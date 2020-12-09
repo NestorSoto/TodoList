bf57bce336b055806e433f215c7998c4d3cd3045
Néstor <66892045+NestorSoto@users.noreply.github.com>
2020-12-09 17:02:51 -0500

    update

--- src/pages/facebook.php
+++ src/pages/facebook.php
@@ -10,7 +10,7 @@ $fb = new Facebook\Facebook([
 $helper = $fb->getRedirectLoginHelper();
 
 $permissions = ['email']; // Optional permissions
-$redirectURL = "https://".$_SERVER['SERVER_NAME']."../../fb-callback.php";
+$redirectURL = "https://".$_SERVER['SERVER_NAME']."/fb-callback.php";
 $loginUrl = $helper->getLoginUrl($redirectURL, $permissions);
 echo '<a href="' . $loginUrl . '">Log in con Facebook!</a>';
 
