442a38078eed270af24dc1398346a02f189e5fff
Néstor <66892045+NestorSoto@users.noreply.github.com>
2020-12-10 17:11:28 -0500

    Update registro.php

--- src/pages/registro.php
+++ src/pages/registro.php
@@ -14,9 +14,9 @@
          $header.="X.Mailer: PHP/". phpversion();
         $msg="Usted se ha registrado en Todo List. "."\r\n";
         $msg="Si usted no realizó la operacion, contactese con nosotros. ";
-        //$mail= mail($correo,$asunto,$msg,$header);
+        //mail($correo,$asunto,$msg,$header);
         //print_r($mail);
-        if(isset($mail)){
+        if(mail($correo,$asunto,$msg,$header)){
             echo"<h4>enviado</h4>";
         }else{
             echo"<h4>error</h4>";
